72207e8f5a5de605095e8d86b64e5ee4465599c4
Slawek Lida <slaweklida@tlen.pl>
2023-06-02 23:15:34 +0200

    powershell command

---
+++ src/main/java/com/slaweklida/atiperaTask/FirstEndpoint.java
@@ -0,0 +1,35 @@
+package com.slaweklida.atiperaTask;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InaccessibleObjectException;
+
+@RestController
+public class FirstEndpoint {
+    @GetMapping("/user")
+    public void getUser() throws IOException {
+        String command = "powershell.exe gh repo list abcslawek";
+        Process powerShellProcess = Runtime.getRuntime().exec(command);
+        powerShellProcess.getOutputStream().close();
+        String line;
+        System.out.println("Standard Output:");
+        BufferedReader stdout = new BufferedReader(new InputStreamReader(
+                powerShellProcess.getInputStream()));
+        while ((line = stdout.readLine()) != null) {
+            System.out.println(line);
+        }
+        stdout.close();
+        System.out.println("Standard Error:");
+        BufferedReader stderr = new BufferedReader(new InputStreamReader(
+                powerShellProcess.getErrorStream()));
+        while ((line = stderr.readLine()) != null) {
+            System.out.println(line);
+        }
+        stderr.close();
+        System.out.println("Done");
+    }
+}
