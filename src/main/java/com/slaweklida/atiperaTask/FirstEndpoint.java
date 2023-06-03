1ef4f4d8187bbec351dd17bb1f71fc9ff947a549
Slawek Lida <slaweklida@tlen.pl>
2023-06-03 10:14:14 +0200

    all my repositories are displayed in the browser

--- src/main/java/com/slaweklida/atiperaTask/FirstEndpoint.java
+++ src/main/java/com/slaweklida/atiperaTask/FirstEndpoint.java
@@ -11,25 +11,27 @@ import java.lang.reflect.InaccessibleObjectException;
 @RestController
 public class FirstEndpoint {
     @GetMapping("/user")
-    public void getUser() throws IOException {
-        String command = "powershell.exe gh repo list abcslawek";
+    public String getUser() throws IOException {
+        String command = "powershell.exe gh repo list abcslawek --source";
         Process powerShellProcess = Runtime.getRuntime().exec(command);
         powerShellProcess.getOutputStream().close();
+        String text = "";
         String line;
         System.out.println("Standard Output:");
         BufferedReader stdout = new BufferedReader(new InputStreamReader(
                 powerShellProcess.getInputStream()));
         while ((line = stdout.readLine()) != null) {
-            System.out.println(line);
+            text += (line + "<br>");
         }
         stdout.close();
         System.out.println("Standard Error:");
         BufferedReader stderr = new BufferedReader(new InputStreamReader(
                 powerShellProcess.getErrorStream()));
         while ((line = stderr.readLine()) != null) {
-            System.out.println(line);
+            text += (line + "<br>");
         }
         stderr.close();
         System.out.println("Done");
+        return text;
     }
 }
